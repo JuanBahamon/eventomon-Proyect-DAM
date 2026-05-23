9fcf1e29e6ee5637b194d407ae35bef3d6fa7e94
Juan Jose Bahamon <juan.bahamon@uao.edu.co>
2026-05-22 19:38:09 -0500

    Primer commit estructura IONIC

---
+++ eventomon-proyect-DAM/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eventomon-proyect-DAM',
+  webDir: 'dist'
+};
+
+export default config;
